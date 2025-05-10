8df4c01b6404b3174d08bc78e6256bbc313647aa
tlundin <terje.lundin@gmail.com>
2025-05-10 13:49:55 +0100

    material menu

--- app/src/main/java/com/teraim/fieldapp/ui/DrawerMenu.java
+++ app/src/main/java/com/teraim/fieldapp/ui/DrawerMenu.java
@@ -1,113 +1,62 @@
 package com.teraim.fieldapp.ui;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.util.SparseArray;
+import android.util.SparseIntArray;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
+import com.google.android.material.navigation.NavigationView;
 import com.teraim.fieldapp.R;
 import com.teraim.fieldapp.Start;
 import com.teraim.fieldapp.dynamic.types.Workflow;
-import com.teraim.fieldapp.ui.DrawerMenuAdapter.RowType;
+import com.teraim.fieldapp.non_generics.Constants;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class DrawerMenu {
+public class DrawerMenu implements NavigationView.OnNavigationItemSelectedListener {
 
 	private final Activity frameActivity;
-	private DrawerMenuAdapter mAdapter;
-	private List<DrawerMenuItem> items;
+	private SubMenu currentSubMenu = null;
 	private DrawerLayout mDrawerLayout;
-	private ListView mDrawerList;
-	private ActionBarDrawerToggle mDrawerToggle;
-	private int currentIndex=0;
-	private SparseArray<Integer> index;
-	private List<Workflow> workflowsL;
+	private ActionBarDrawerToggle toggle;
+	private int nextItemId = 1000;
+	private SparseIntArray index;
+	private Map<Integer, Workflow> workflowMap = new HashMap<>();
 	private boolean open = false;
+	private NavigationView navigationView;
 
-	public DrawerMenu(Activity a) {
+
+	public DrawerMenu(Activity a, Toolbar toolbar) {
 		frameActivity=a;
-		createMenu();
+		this.createMenu(toolbar);
 	}
 
-	private class DrawerItemClickListener implements ListView.OnItemClickListener {
-		//private boolean firstTimeClick=true;
 
-		@Override
-		public void onItemClick(AdapterView parent, View view, int position, long id) {
-			/*
-			if (firstTimeClick) {
-				// Create new fragment and transaction
-				Fragment newFragment = new BackgroundFragment();
-				FragmentTransaction transaction = frameActivity.getFragmentManager().beginTransaction();
-				// Replace whatever is in the fragment_container view with this fragment,
-				// and add the transaction to the back stack
-				transaction.replace(R.id.content_frame, newFragment);			
-				// Commit the transaction
-				transaction.commit();
-				firstTimeClick=false;
 
-			}
-			*/
-			if (items.get(position).getViewType()!=RowType.HEADER_ITEM.ordinal()) {				
-				selectItem(position);
-			} else {
-				Log.d("nils","header selected. no action");
-			}
-		}
 
-		/** Swaps fragments in the main content view */
-		private void selectItem(int position) {
-
-			// Highlight the selected item, update the title, and close the drawer
-			mDrawerList.setItemChecked(position, true);
-			//String wfId = mapItemsToName.get(position);
-			Integer pos = index.get(position);
-			if (pos!=null) {
-				Workflow wf = workflowsL.get(pos);
-				//Workflow wf = gs.getWorkflowFromLabel(wfId);
-				if (wf == null)
-					Log.e("vortex","ups!!! Got null when looking for workflow ");
-				else {
-					// Create a new fragment and specify the  to show based on position
-					DrawerMenu.this.closeDrawer();
-					// Insert the fragment by replacing any existing fragment
-					//TODO: This might not work...might need to know statusvariable.
-					Start.singleton.changePage(wf,null);
-				}
-			} else
-				Log.e("vortex","Could not find any entry for menu position "+position);
-		}
-	}
-	public boolean isDrawerOpen() {
-		return open;
-	}
-	private void createMenu() {
-		//drawer items
-		//different if already created?
-		items = new ArrayList<DrawerMenuItem>(); 
-		mAdapter = new DrawerMenuAdapter(frameActivity, items);
+	void createMenu(Toolbar toolbar) {
 
 		mDrawerLayout = frameActivity.findViewById(R.id.drawer_layout);
-		mDrawerList = frameActivity.findViewById(R.id.left_drawer);
+		navigationView  = frameActivity.findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
 
-		// Set the adapter for the list view
-		mDrawerList.setAdapter(mAdapter);
-		// Set the list's click listener
-		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-		mDrawerToggle = new ActionBarDrawerToggle(
+		toggle = new ActionBarDrawerToggle(
 				frameActivity,                  /* host Activity */
-				mDrawerLayout,         /* DrawerLayout object */
-				  /* nav drawer icon to replace 'Up' caret */
+				mDrawerLayout, /* DrawerLayout object */
+				toolbar,  /* nav drawer icon to replace 'Up' caret */
 				R.string.drawer_open,  /* "open drawer" description */
 				R.string.drawer_close  /* "close drawer" description */
 				) {
@@ -115,6 +64,7 @@ public class DrawerMenu {
 			/** Called when a drawer has settled in a completely closed state. */
 			public void onDrawerClosed(View view) {
 				super.onDrawerClosed(view);
+				Log.d("axion","drawer closed");
 				open = false;
 			}
 
@@ -125,53 +75,93 @@ public class DrawerMenu {
 				//createDrawerMenu(wfs);
 				//mAdapter.notifyDataSetChanged();				
 				super.onDrawerOpened(drawerView);
+				Log.d("axion","drawer opened");
 				open=true;
 			}
 
 		};
-
-		// Set the drawer toggle as the DrawerListener
-		mDrawerLayout.addDrawerListener(mDrawerToggle);
-
-		mAdapter.notifyDataSetChanged();				
-
-		workflowsL = new ArrayList<Workflow>();
-		index = new SparseArray<>();
-		currentIndex=0;
+		mDrawerLayout.addDrawerListener(toggle);
+		toggle.syncState();
 
 	}
 
 	public void addHeader(String label, int bgColor,int textColor) {
-		items.add(new DrawerMenuHeader(label,bgColor,textColor));
+		Menu menu = navigationView.getMenu();
+		currentSubMenu = menu.addSubMenu(Menu.NONE, Menu.NONE, Menu.NONE, label);
 	}
 
 	public void addItem(String label, Workflow wf,int bgColor,int textColor) {
-		//add the workflow reference to a list
-		workflowsL.add(wf);
-		//keep track of the location.
-		index.put(items.size(),currentIndex++);
-		items.add(new DrawerMenuSelectable(label,bgColor,textColor));
+		if (currentSubMenu == null) {
+			// Handle cases where addItem is called before addHeader, maybe add to a default section or throw an error
+			// For this example, let's add a default header if none exists
+			addHeader("Vecka "+Constants.getWeekNumber(), android.R.color.transparent, android.R.color.black);
+		}
 
+		int itemId = nextItemId++;
+		MenuItem menuItem = currentSubMenu.add(Menu.NONE, itemId, Menu.NONE, label);
+		menuItem.setCheckable(true); // Make items selectable
+
+		// Store the workflow object
+		workflowMap.put(itemId, wf);
 	}
 
+	private MenuItem findFirstSelectableItem(Menu menu) {
+		for (int i = 0; i < menu.size(); i++) {
+			MenuItem item = menu.getItem(i);
+			if (item.hasSubMenu()) {
+				SubMenu subMenu = item.getSubMenu();
+				for (int j = 0; j < subMenu.size(); j++) {
+					MenuItem subMenuItem = subMenu.getItem(j);
+					if (subMenuItem.isCheckable()) {
+						return subMenuItem;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	@Override
+	public boolean onNavigationItemSelected(MenuItem item) {
+		// Handle navigation view item clicks.
+		// You can retrieve the associated Workflow object using workflowMap
+		Workflow workflow = workflowMap.get(item.getItemId());
+		if (workflow != null) {
+			// Use the workflow object to determine which fragment to load
+			Start.singleton.changePage(workflow,null);
+			// Highlight the selected item
+			navigationView.setCheckedItem(item.getItemId());
+
+		} else
+			Log.e("vortex","ups!!! Got null when looking for workflow ");
+
+		mDrawerLayout.closeDrawer(GravityCompat.START);
+		return true;
+	}
+
+	public boolean isDrawerOpen() {
+		return open;
+	}
 	public void closeDrawer() {
+		Log.d("axion","closing drawer");
 		mDrawerLayout.closeDrawers();
 	}
 
 	public void openDrawer() {	
-		mAdapter.notifyDataSetChanged();				
-		mDrawerLayout.openDrawer(Gravity.LEFT);	
+		toggle.syncState();
+		mDrawerLayout.openDrawer(GravityCompat.START);
 	}
 
 
 	public ActionBarDrawerToggle getDrawerToggle() {
 		// TODO Auto-generated method stub
-		return mDrawerToggle;
+		return toggle;
 	}
 
 	public void clear() {
-		items.clear();
-		mAdapter.notifyDataSetChanged();				
+		navigationView.getMenu().clear(); // Clear existing menu items
+		currentSubMenu = null; // Reset current submenu
+		workflowMap.clear(); // Clear the workflow map
+		nextItemId = 1000; // Reset item ID counter
 	}
 
 
